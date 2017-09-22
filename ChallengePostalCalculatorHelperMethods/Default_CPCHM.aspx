1fcd47051ca0d108ec7c4b7e7c437bcb26187212
goyaschooling <goyaschooling@gmail.com>
2017-09-22 11:11:15 -0700

    Add All Files

---
+++ ChallengePostalCalculatorHelperMethods/Default_CPCHM.aspx
@@ -0,0 +1,36 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="Default_CPCHM.aspx.cs" Inherits="ChallengePostalCalculatorHelperMethods.Default_CPCHM" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div>
+            Postal Calculator<br />
+            <br />
+            Width:&nbsp;&nbsp;
+            <asp:TextBox ID="inputWidthBox" runat="server" AutoPostBack="True" OnTextChanged="InputWidthBox_TextChanged"></asp:TextBox>
+            <br />
+            Height:&nbsp;
+            <asp:TextBox ID="inputHeightBox" runat="server" AutoPostBack="True" OnTextChanged="InputHeightBox_TextChanged"></asp:TextBox>
+            <br />
+            Length:&nbsp;
+            <asp:TextBox ID="inputLengthBox" runat="server" AutoPostBack="True" OnTextChanged="InputLengthBox_TextChanged"></asp:TextBox>
+            <br />
+            <br />
+            <asp:RadioButton ID="radioBtnGround" runat="server" AutoPostBack="True" GroupName="shipMethod" OnCheckedChanged="RadioBtnGround_CheckedChanged" Text="Ground Shipping" />
+            <br />
+            <asp:RadioButton ID="radioBtnAir" runat="server" AutoPostBack="True" GroupName="shipMethod" OnCheckedChanged="RadioBtnAir_CheckedChanged" Text="Air Shipping" />
+            <br />
+            <asp:RadioButton ID="radioBtnOvernight" runat="server" AutoPostBack="True" GroupName="shipMethod" OnCheckedChanged="RadioBtnOvernight_CheckedChanged" Text="Next Day Shipping" />
+            <br />
+            <br />
+            <br />
+            <asp:Label ID="shipmentTotalOutput" runat="server"></asp:Label>
+        </div>
+    </form>
+</body>
+</html>
---
+++ ChallengePostalCalculatorHelperMethods/Default_CPCHM.aspx.cs
@@ -0,0 +1,199 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace ChallengePostalCalculatorHelperMethods
+{
+    public partial class Default_CPCHM : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if (!IsPostBack)
+            {
+                inputHeightBox.Text = string.Empty;
+                inputLengthBox.Text = string.Empty;
+                inputWidthBox.Text = string.Empty;
+            }
+        }
+        
+        protected void InputWidthBox_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void InputHeightBox_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void InputLengthBox_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+        
+        protected void RadioBtnGround_CheckedChanged(object sender, EventArgs e)
+        {
+            CalculateVolume();
+            CalculatePost(.15);
+        }
+
+        protected void RadioBtnAir_CheckedChanged(object sender, EventArgs e)
+        {
+            CalculateVolume();
+            CalculatePost(.25);
+        }
+
+        protected void RadioBtnOvernight_CheckedChanged(object sender, EventArgs e)
+        {
+            CalculateVolume();
+            CalculatePost(.45);
+            //shipmentTotalOutput.Text = CalculatePost();
+        }
+
+        //=================================================
+        //  Method to calculate volume  - CalculateVolume()
+        //=================================================
+
+        public double CalculateVolume()
+        {
+            double totalVolume;
+
+            if (inputLengthBox.Text.Trim().Length == 0)
+            {
+                totalVolume = double.Parse(inputWidthBox.Text);
+                totalVolume = totalVolume * (double.Parse(inputHeightBox.Text));
+                
+                //shipmentTotalOutput.Text = totalVolume.ToString();
+                return totalVolume;
+            }
+            else
+            //else if (!String.IsNullOrEmpty(inputLengthBox.Text))
+            {
+                totalVolume = double.Parse(inputWidthBox.Text);
+                totalVolume = totalVolume * (double.Parse(inputHeightBox.Text));
+                totalVolume = totalVolume * (double.Parse(inputLengthBox.Text));
+                
+                shipmentTotalOutput.Text = totalVolume.ToString();
+                return totalVolume;
+            }
+            //return NewMethod(totalVolume);
+            //object totalvolume = null;
+
+            //return CalculatePost(totalVolume);
+
+        }
+
+        //=================================================
+        // Method to calculate postage - CalculatePost();
+        //=================================================
+        
+        /*public double CalculatePost(Action calculateVolume, double multiplier)
+        {
+            if (calculateVolume == null)
+            {
+                throw new ArgumentNullException(nameof(calculateVolume));
+            }
+            */
+        public double CalculatePost(double multiplier)
+        {
+            double totalPostage;
+            double totalVolume = CalculateVolume(); //variable = output from method
+
+            if (radioBtnGround.Checked)
+            {
+                totalPostage = totalVolume * multiplier;
+                shipmentTotalOutput.Text = string.Format("Your Parcel will cost {0:C} to ship.", totalPostage);
+                //shipmentTotalOutput.Text = totalPostage.ToString();
+                return totalPostage;
+            }
+
+            else if (radioBtnAir.Checked)
+            {
+                totalPostage = totalVolume * multiplier;
+                shipmentTotalOutput.Text = string.Format("Your Parcel will cost {0:C} to ship.", totalPostage);
+                //shipmentTotalOutput.Text = string.Format("{0:C}", totalPostage);
+                return totalPostage;
+            }
+
+            else if (radioBtnOvernight.Checked)
+            {
+                totalPostage = totalVolume * multiplier;
+                shipmentTotalOutput.Text = string.Format("Your Parcel will cost {0:C} to ship.", totalPostage);
+                return totalPostage;
+            }
+            else
+                return 0;
+        }
+        
+
+        /*
+        public double CalculatePost()//double totalPostage, double totalVolume)
+        {
+
+            if (radioBtnGround.Checked)
+            {
+                totalPostage = totalVolume * .15;
+                return totalPostage;
+            }
+
+            else if (radioBtnAir.Checked)
+            {
+                totalPostage = totalVolume * .25;
+                return totalPostage;
+            }
+
+            else if (radioBtnOvernight.Checked)
+            {
+                totalPostage = totalVolume * .45;
+            }
+            else
+                return totalPostage;
+
+            shipmentTotalOutput.Text = totalPostage.ToString();
+        }
+        */
+
+        /*
+
+        double totalPostage = totalVolume * multiplier;
+        
+        protected void RadioBtnGround_CheckedChanged(object sender, EventArgs e)
+        CalculatePost(.25);
+        
+        protected void RadioBtnAir_CheckedChanged(object sender, EventArgs e)
+        CalculatePost(.35);
+        
+        protected void RadioBtnOvernight_CheckedChanged(object sender, EventArgs e)
+        CalculatePost(.40);
+        */
+
+        /*
+        if (radioBtnGround.Checked) return .15;
+        else if (radioBtnAir.Checked) return .25;
+        else if (radioBtnOvernight.Checked) return .45;
+        else return 0;
+
+
+
+
+
+
+        //shipmentTotalOutput.Text = totalVolume.ToString();
+
+
+        //shipmentTotalOutput.Text = "Your Parcel will cost " + totalPostage.ToString() + " to ship";
+
+        //Your parcel will cost $xx to ship.
+
+
+
+        */
+
+    }
+
+
+}
+
